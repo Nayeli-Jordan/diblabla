8b08b96e07510efd171b3e5aa6c43f21538ad1e7
Nayeli Jordan <[nayeli@queonda.com.mx]>
2018-10-15 13:30:54 -0500

    actualización de local con ftp

---
+++ themes/diblabla/page-blog-side-bar-social.php
@@ -0,0 +1,5 @@
+<div class="sidebar-social">
+	<a href="https://www.facebook.com/Diblabla/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+	<a href="https://www.instagram.com/diblablacomunicacion/?hl=es" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+	<a href="https://www.youtube.com/channel/UCODkX4cAU8EZASKbfgzdSdw?view_as=subscriber" target="_blank"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+</div>
